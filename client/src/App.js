9bd84d72790e856a26882641b1f2427537d5134b
Nesrine <l.nesrine155@gmail.com>
2024-09-24 21:58:04 -0400

    Add React routing for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./Pages/home_page";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/home_page'
+import Introduction from './Pages/Introduction'
+import Login from './Pages/Login'
+
 import "bootstrap/dist/css/bootstrap.min.css";
+// import Resources from './Resources';
 
 function App() {
-  
-
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/introduction" element={<Introduction />} />
+      <Route path="/login" element={<Login />} />
+      {/* <Route path="/resources" element={<Resources />} /> */}
+    </Routes>
   );
 }
 
