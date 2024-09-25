31ea2fb8f26d6c2a90dc09c870c88c16654d72d8
R-Mauricio13 <r_mau@live.concordia.ca>
2024-09-25 16:17:44 -0400

    Merged Nesrine's work into project

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './Pages/home_page'
-import Introduction from './Pages/Introduction'
-import Login from './Pages/Login'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/home_page";
+import Introduction from "./Pages/Introduction";
+import Login from "./Pages/Login";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 // import Resources from './Resources';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/introduction" element={<Introduction />} />
-      <Route path="/login" element={<Login />} />
-      {/* <Route path="/resources" element={<Resources />} /> */}
-    </Routes>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/introduction" element={<Introduction />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/resources" element={<Resources />} /> */}
+      </Routes>
+ 
   );
 }
 
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import '../Styles/Header.css'
-import concordiaLogo from '../Assets/concordia-logo.png';
+import React from "react";
+import "../Styles/Header.css";
+import concordiaLogo from "../Assets/concordia-logo.png";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  let navigate = useNavigate();
+
+  function returnHome() {
+    navigate("/");
+  }
+
   return (
     <header className="header">
       <div className="container">
         <div className="logo-container">
-          <img src={concordiaLogo} alt="Concordia University Logo" className="logo" />
+          <button className="logo-container" onClick={returnHome}>
+            <img
+              src={concordiaLogo}
+              alt="Concordia University Logo"
+              className="logo"
+            />
+          </button>
           <h1>Welcome to the Peer Assessment System</h1>
         </div>
       </div>
