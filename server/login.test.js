7bc7d664fa77e88ff7953dec2fa0a653b82333b5
YeJimmy <jimmyye2000@gmail.com>
2024-11-08 23:04:55 -0500

    Finished backend unit test for login

--- server/login.test.js
+++ server/login.test.js
@@ -18,7 +18,7 @@ jest.mock('mysql2', () => ({
 afterEach(cleanup);
 
 describe("Login endpoint", () => { 
-    test('should return status code 200', async () => {
+    test('test login as student', async () => {
         const mockQuery = createConnection().query;
 
         mockQuery.mockImplementation((sql, params, callback) => {
@@ -29,6 +29,45 @@ describe("Login endpoint", () => {
 
         expect(response.status).toBe(302);
         expect(response.text).toBe("Found. Redirecting to http://localhost:3000/Student_Login");
+    })
+
+    test('test login as instructor', async () => {
+        const mockQuery = createConnection().query;
+
+        mockQuery.mockImplementation((sql, params, callback) => {
+            callback(null, [{id: 1}]);
+        });
+
+        const response = await request(app).get('/login?username=Jimmy_Ye&password=jy&user_role=instructor');
+
+        expect(response.status).toBe(302);
+        expect(response.text).toBe("Found. Redirecting to http://localhost:3000/Instructor_Login");
+    })
+
+    test('test login with no role', async () => {
+        const mockQuery = createConnection().query;
+
+        mockQuery.mockImplementation((sql, params, callback) => {
+            callback(null, [{id: 1}]);
+        });
+
+        const response = await request(app).get('/login?username=Jimmy_Ye&password=jy&user_role=');
+
+        expect(response.status).toBe(302);
+        expect(response.text).toBe("Found. Redirecting to http://localhost:3000/?error-msg=Invalid%20user%20role");
+    })
+
+    test('test login with invalid return', async () => {
+        const mockQuery = createConnection().query;
+
+        mockQuery.mockImplementation((sql, params, callback) => {
+            callback(null, []);
+        });
+
+        const response = await request(app).get('/login?username=Jimmy_Y&password=jy&user_role=student');
+
+        expect(response.status).toBe(302);
+        expect(response.text).toBe("Found. Redirecting to http://localhost:3000/?error-msg=Invalid%20username%20or%20password");
     })
 });
 
