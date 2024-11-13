63bf3143647d684eaaf3640612ecc1c0cb647079
YeJimmy <jimmyye2000@gmail.com>
2024-11-13 00:59:12 -0500

    Fixing previous attempt

--- client/src/UnitTest/PeerReview.test.js
+++ client/src/UnitTest/PeerReview.test.js
@@ -93,7 +93,6 @@ describe('PeerReview Component', () => {
     expect(screen.getByLabelText('work ethic 3')).toBeChecked();
 
     mockAxios.onPost('http://localhost:8080/submit_review').reply(200, { success: true });
-    });
 
     const form = screen.getByTestId('peer-review-form');
     fireEvent.submit(form); // Trigger the form submission
